fa51ae16b1d2119a3a5458c649344de44e7862d6
jr635 <jr635@njit.edu>
2020-09-17 12:16:18 -0400

    HW1 commit

---
+++ IT114HW1/src/HW1.java
@@ -0,0 +1,13 @@
+public class Homework {
+	public static void main(String[] args) {
+		String name;
+		name = "Jakub";
+		System.out.println(name);
+
+		int age = 14;
+		System.out.println(age);
+
+		double decimal = 0.72;
+		System.out.println(decimal);
+	}
+}
